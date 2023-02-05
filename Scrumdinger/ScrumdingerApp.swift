bfe96ec510bd45e3dbb42f3cf9add97c1aaa9c8c
jungguen89 <leral3@nate.com>
2023-02-05 16:07:42 +0900

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by 권정근 on 2023/02/05.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
